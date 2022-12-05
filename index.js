fa40bf9a4004f2393ccfaf2fba7d0d96870da360
Nicolás Bentancor <bentanico@gmail.com>
2022-12-05 20:00:33 -0300

    patients
    models, routing and controllers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
 import veterinarioRoutes from "./routes/veterinarioRoutes.js";
+import pacienteRoutes from "./routes/pacienteRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -11,9 +12,10 @@ dotenv.config();
 conectarDB();
 
 app.use("/api/veterinarios", veterinarioRoutes);
+app.use("/api/pacientes", pacienteRoutes);
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor funcionando en el puerto ${PORT}`);
+    console.log(`Servidor funcionando en el puerto ${PORT}`);
 });
